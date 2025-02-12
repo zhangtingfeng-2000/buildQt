c1340d1d8daf2bf932347217e112510c30ffcfdc
yuanpeirong <yuanpeirong@vip.qq.com>
2025-02-12 15:15:24 +0800

    完成Qt6 MSVC2022版本

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# buildQt
-从源代码构建Qt
+# buildQt 从源代码自动构建Qt
+
+## 说明
+1. 本项目的主要目的是从源代码构建Qt的二进制版本(使用GithubActions自动构建)
+2. 主要针对开源版本的Qt5、Qt6版本
+3. 主要构建Windows版本(使用MSVC、MinGW64、LLVM-MinGW三大编译器)
+4. 主要构建static版本
+5. 主要构建Release版本
+
+## 版本
+1. 无"RP"版本：从Qt完整源代码构建，构建绝大部分组件
+2. 有"RP"版本：从Qt组件源代码构建，目前只构建qtbase、qttools、qttranslations、qtsvg共4个组件
+
+## 仓库地址
+1. Github: https://github.com/yuanpeirong/buildQt
+
+## 目前已开放版本
+1. Qt6 msvc2022_64
+2. Qt6 msvc2022_64_RP
+3. Qt6其他编译器版本、Qt5所有版本正在测试中，完成后上传
---
+++ buildQt6/build-Qt6msvc2022_64.cmd
@@ -0,0 +1,45 @@
+@chcp 65001
+@cd /d %~dp0
+
+:: 设置Qt版本
+SET QT_VERSION=6.8.2
+
+:: 设置MSVC版本代号
+SET MSVC_VERSION=msvc2022_64
+
+:: 设置MSVC2022环境
+CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
+
+:: 设置Qt文件夹路径
+SET QT_PATH=D:\a\buildQt\Qt
+
+::----------以下无需修改----------
+
+:: 设置Qt源代码目录
+SET SRC_QT="%QT_PATH%\%QT_VERSION%\qt-everywhere-src-%QT_VERSION%"
+
+:: 设置安装文件夹目录
+SET INSTALL_DIR="%QT_PATH%\%QT_VERSION%-static\%MSVC_VERSION%"
+
+:: 设置build文件夹目录
+SET BUILD_DIR="%QT_PATH%\%QT_VERSION%\build-%MSVC_VERSION%"
+
+:: 根据需要进行全新构建
+rmdir /s /q "%BUILD_DIR%"
+:: 定位到构建目录：
+mkdir "%BUILD_DIR%" && cd /d "%BUILD_DIR%"
+
+:: configure
+call %SRC_QT%\configure.bat -static -static-runtime -release -prefix %INSTALL_DIR% -nomake examples -nomake tests -skip qtwebengine -opensource -confirm-license -qt-libpng -qt-libjpeg -qt-zlib -qt-pcre -qt-freetype -schannel -platform win32-msvc
+
+:: 编译(不要忘记点)
+cmake --build . --parallel
+
+:: 安装(不要忘记点)
+cmake --install .
+
+::复制qt.conf
+copy %~dp0\qt.conf %INSTALL_DIR%\bin
+
+::@pause
+@cmd /k cd /d %INSTALL_DIR%
