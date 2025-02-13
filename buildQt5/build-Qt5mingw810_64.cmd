290859d713ce2a0d8ccad55b5306c843640848ab
yuanpeirong <yuanpeirong@vip.qq.com>
2025-02-13 20:48:20 +0800

    修复：-opengl desktop

--- buildQt5/build-Qt5mingw810_64.cmd
+++ buildQt5/build-Qt5mingw810_64.cmd
@@ -36,7 +36,7 @@ rmdir /s /q "%BUILD_DIR%"
 mkdir "%BUILD_DIR%" && cd /d "%BUILD_DIR%"
 
 :: configure
-call %SRC_QT%\configure.bat -static -static-runtime -release -prefix %INSTALL_DIR% -nomake examples -nomake tests -skip qtwebengine -opensource -confirm-license -qt-libpng -qt-libjpeg -qt-zlib -qt-pcre -qt-freetype -schannel -platform win32-g++
+call %SRC_QT%\configure.bat -static -static-runtime -release -prefix %INSTALL_DIR% -nomake examples -nomake tests -skip qtwebengine -opensource -confirm-license -qt-libpng -qt-libjpeg -qt-zlib -qt-pcre -qt-freetype -schannel -opengl desktop -platform win32-g++
 
 :: 编译、安装
 mingw32-make -j%NUM_THRED%         
